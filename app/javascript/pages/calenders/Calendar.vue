434fb774ade6b1d16b44fb2f60655cafa8d5a427
sekken11 <soratorobo0611@gmail.com>
2023-10-14 18:51:14 +0900

    add:fullCalender_02

--- app/javascript/pages/calenders/Calendar.vue
+++ app/javascript/pages/calenders/Calendar.vue
@@ -1,8 +1,10 @@
 <template>
 <div class="bg-white rounded shadow p-3 mb-3">
+  <div class="mb-4">
+    <v-btn block color="success" @click="eventChange">表示切替</v-btn>
+  </div>
   <FullCalendar :options='calendarOptions'
   />
-  {{test}}
 </div>
 
 <GachaRecordPart :gachas="daysGachas" :perPage="perPage">日付が選択されていません</GachaRecordPart>
@@ -24,6 +26,7 @@ export default {
   },
   data() {
     return {
+      event_type: 'gacha_price',
       perPage: 5,
       date: '2023-10-12',
       calendarOptions: {
@@ -36,15 +39,15 @@ export default {
         },
         eventClick: (info) => {
           this.daySelect(info.event.start)
+          this.test = info
         },
         headerToolbar: {
           left: 'prev',
           center: 'title',
           right: 'next'
         },
-        eventColor: 'rightblue',
-        eventClassNames: ['text-center', 'mx-3'],
-        dayHeaderClassNames: [''],
+        eventClassNames: ['text-center'],
+        fixedWeekCount: false,
       }
     }
   },
@@ -72,25 +75,51 @@ export default {
       this.$router.push({ name: 'GachaRecordCreate' })
     },
     calendarEvents() {
-      var array = [{ title: 0, start: this.gachas[0].date }]
-      var int = 0
-      this.gachas.forEach(gacha => {
-        if (array[int].start == gacha.date) {
-          array[int].title += gacha.count
-        } else if (array[int].start != gacha.date) {
-          if (!array[int + 1]) {
-            int += 1
-            array.push({ title: gacha.count, start: gacha.date })
+      if (this.event_type == 'gacha_count') {
+        var array = [{ title: 0, start: this.gachas[0].date }]
+        var int = 0
+        this.gachas.forEach(gacha => {
+          if (array[int].start == gacha.date) {
+            array[int].title += gacha.count
+          } else if (array[int].start != gacha.date) {
+            if (!array[int + 1]) {
+              int += 1
+              array.push({ title: gacha.count, start: gacha.date })
+            }
           }
-        }
-      });
-      array.forEach(gacha => {
-        gacha.title = gacha.title + '回'
-      })
+        });
+        array.forEach(gacha => {
+          gacha.title = gacha.title + '回'
+        })
+      } else if (this.event_type == 'gacha_price') {
+        var array = [{ title: 0, start: this.gachas[0].date }]
+        var int = 0
+        this.gachas.forEach(gacha => {
+          if (array[int].start == gacha.date) {
+            array[int].title += Math.round(gacha.currency_package.price/gacha.currency_package.quantity*gacha.currency_package.need_one_gacha_stones*gacha.count)
+          } else if (array[int].start != gacha.date) {
+            if (!array[int + 1]) {
+              int += 1
+              array.push({ title: Math.round(gacha.currency_package.price/gacha.currency_package.quantity*gacha.currency_package.need_one_gacha_stones*gacha.count), start: gacha.date })
+            }
+          }
+        });
+        array.forEach(gacha => {
+          gacha.title = gacha.title + '円'
+        })
+      }
       this.calendarOptions.events = array
     },
     daySelect(day) {
       this.date = day.toLocaleDateString('sv-SE')
+    },
+    eventChange() {
+      if (this.event_type == 'gacha_count') {
+        this.event_type = 'gacha_price'
+      } else {
+        this.event_type = 'gacha_count'
+      }
+      this.calendarEvents()
     },
   },
 }
