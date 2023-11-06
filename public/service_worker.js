81c4d3fe28a7bf67caa55cd7405ec3d2dcf241b4
sekken11 <soratorobo0611@gmail.com>
2023-11-06 20:28:26 +0900

    add:PWA

---
+++ public/service_worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'gacha_diary_cache';
+const urlsToCache = [
+  '/',
+  '/info',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
