463284ccacc0b6045c9a76f635a0d04a9da1263c
sekken11 <soratorobo0611@gmail.com>
2023-11-10 06:17:57 +0900

    fix:validates

--- app/models/currency_package.rb
+++ app/models/currency_package.rb
@@ -2,4 +2,9 @@ class CurrencyPackage < ApplicationRecord
   belongs_to :user
   has_many :gachas, dependent: :nullify
   has_many :user_posses_stones, dependent: :nullify
+
+  validates :name, presence: true
+  validates :need_one_gacha_stones, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
 end
--- app/models/gacha.rb
+++ app/models/gacha.rb
@@ -1,4 +1,6 @@
 class Gacha < ApplicationRecord
   belongs_to :user
   belongs_to :currency_package
+
+  validates :date, presence: true
 end
