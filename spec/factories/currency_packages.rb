564cee198fc0970aba96b2c6df2bc20f167a10a4
sekken11 <soratorobo0611@gmail.com>
2023-11-18 03:06:46 +0900

    fix:test_factory_bot

--- spec/factories/currency_packages.rb
+++ spec/factories/currency_packages.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
     need_one_gacha_stones { Faker::Number.between(from: 150, to: 300) }
     price { Faker::Number.between(from: 100, to: 200) }
     quantity { Faker::Number.between(from: 100, to: 200) }
-    association :user
   end
 end
